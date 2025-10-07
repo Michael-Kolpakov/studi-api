1813ff5a7f2dc7071bf33e4956f5da231dd77b8e
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-08 02:38:15 +0300

    Implemented basic version of DbContext

---
+++ Teachio.DAL/Persistence/TeachioDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Teachio.DAL.Persistence;
+
+public class TeachioDbContext : DbContext
+{
+    public TeachioDbContext()
+    {
+    }
+
+    public TeachioDbContext(DbContextOptions<TeachioDbContext> options)
+        : base(options)
+    {
+    }
+}
