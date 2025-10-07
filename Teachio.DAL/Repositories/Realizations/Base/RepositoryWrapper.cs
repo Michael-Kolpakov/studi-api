94204041df3337114cb70c5056bb025b66093b17
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-08 02:40:46 +0300

    Implemented realizations for base repository interfaces

---
+++ Teachio.DAL/Repositories/Realizations/Base/RepositoryWrapper.cs
@@ -0,0 +1,24 @@
+﻿using Teachio.DAL.Persistence;
+using Teachio.DAL.Repositories.Interfaces.Base;
+
+namespace Teachio.DAL.Repositories.Realizations.Base;
+
+public class RepositoryWrapper : IRepositoryWrapper
+{
+    private readonly TeachioDbContext _dbContext;
+
+    public RepositoryWrapper(TeachioDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public int SaveChanges()
+    {
+        return _dbContext.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+}
