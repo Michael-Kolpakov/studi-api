929aa947ba6b05abfe59ba2f82030aafaa57f04e
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-11 00:20:01 +0300

    Implemented DateValidatorAttribute model validation attribute

---
+++ Teachio.DAL/Validators/DateValidatorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teachio.DAL.Validators;
+
+public class DateValidatorAttribute : ValidationAttribute
+{
+    private DateTime MinDate { get; }
+
+    private DateTime MaxDate { get; }
+
+    public DateValidatorAttribute(string minDate, string maxDate)
+    {
+        MinDate = DateTime.Parse(minDate);
+        MaxDate = DateTime.Parse(maxDate);
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        if (value is not DateTime date)
+        {
+            return new ValidationResult("Invalid date format.");
+        }
+
+        if (date < MinDate || date > MaxDate)
+        {
+            return new ValidationResult(
+                $"The field {validationContext.DisplayName} must be between {MinDate:d} and {MaxDate:d}.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
