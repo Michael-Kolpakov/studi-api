32e860a4ea99348e4860bf18619a135d46f7ac7e
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-13 23:57:42 +0300

    Created contract for ILoggerService

---
+++ Teachio.BLL/Services/Interfaces/ILoggerService.cs
@@ -0,0 +1,12 @@
+﻿namespace Teachio.BLL.Services.Interfaces;
+
+public interface ILoggerService
+{
+    void LogInformation(string message);
+
+    void LogWarning(string message);
+
+    void LogDebug(string message);
+
+    void LogError(object? request, string errorMessage);
+}
