04cd033734ea94ac72bfb980fdcced46a9885351
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-13 23:58:14 +0300

    Implemented LoggerService

---
+++ Teachio.BLL/Services/Realizations/LoggerService.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using Teachio.BLL.Services.Interfaces;
+
+namespace Teachio.BLL.Services.Realizations;
+
+public class LoggerService : ILoggerService
+{
+    private readonly ILogger _logger;
+
+    public LoggerService(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogInformation(string message)
+    {
+        _logger.Information(message);
+    }
+
+    public void LogWarning(string message)
+    {
+        _logger.Warning(message);
+    }
+
+    public void LogDebug(string message)
+    {
+        _logger.Debug(message);
+    }
+
+    public void LogError(object? request, string errorMessage)
+    {
+        if (request is not null)
+        {
+            var requestType = request.GetType().ToString();
+            var requestClass = requestType.Substring(requestType.LastIndexOf('.') + 1);
+
+            _logger.Error($"'{requestClass}' handled with the error: {errorMessage}");
+        }
+        else
+        {
+            _logger.Error(errorMessage);
+        }
+    }
+}
