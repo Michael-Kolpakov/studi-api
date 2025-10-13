8e7673be32165d3349c2afabfc11d74471f4ad44
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-13 23:51:55 +0300

    Created repository contracts for existing entities

---
+++ Teachio.DAL/Repositories/Interfaces/Courses/Videos/VideoProgress/IVideoProgressRepository.cs
@@ -0,0 +1,5 @@
+﻿using Teachio.DAL.Repositories.Interfaces.Base;
+
+namespace Teachio.DAL.Repositories.Interfaces.Courses.Videos.VideoProgress;
+
+public interface IVideoProgressRepository : IRepositoryBase<Entities.Courses.Videos.VideoProgress.VideoProgress>;
