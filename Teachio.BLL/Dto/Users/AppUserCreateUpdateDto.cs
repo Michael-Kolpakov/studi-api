80efea74338ee26cd86d4b87cabbfeb5d7db463e
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-14 23:12:07 +0300

    Updated AppUser dtos with DataAnnotations validation

--- Teachio.BLL/Dto/Users/AppUserCreateUpdateDto.cs
+++ Teachio.BLL/Dto/Users/AppUserCreateUpdateDto.cs
@@ -1,10 +1,18 @@
-﻿namespace Teachio.BLL.Dto.Users;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teachio.BLL.Dto.Users;
 
 public abstract class AppUserCreateUpdateDto
 {
+    [Required(ErrorMessage = "'{0}' field is required")]
+    [StringLength(20, ErrorMessage = "Length of '{0}' must be not longer than {1} characters")]
     public string? Name { get; set; }
 
+    [Required(ErrorMessage = "'{0}' field is required")]
+    [StringLength(30, ErrorMessage = "Length of '{0}' must be not longer than {1} characters")]
     public string? Surname { get; set; }
 
+    [Required(ErrorMessage = "'{0}' field is required")]
+    [StringLength(20, ErrorMessage = "Length of '{0}' must be not longer than {1} characters")]
     public string? Role { get; set; }
 }
--- Teachio.BLL/Dto/Users/Update/AppUserUpdateDto.cs
+++ Teachio.BLL/Dto/Users/Update/AppUserUpdateDto.cs
@@ -1,6 +1,9 @@
-﻿namespace Teachio.BLL.Dto.Users.Update;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teachio.BLL.Dto.Users.Update;
 
 public class AppUserUpdateDto : AppUserCreateUpdateDto
 {
+    [Required(ErrorMessage = "'{0}' field is required")]
     public Guid Id { get; set; }
 }
