90cc0bb3bff109f0c0973bc16f7b008b2c876737
Michael-Kolpakov <mihailkolp@gmail.com>
2025-10-16 08:43:03 +0300

    Enriched Serilog log template with additional information

--- Teachio.WebApi/Extensions/SerilogExtension.cs
+++ Teachio.WebApi/Extensions/SerilogExtension.cs
@@ -6,7 +6,7 @@ namespace Teachio.WebApi.Extensions;
 
 public static class SerilogExtension
 {
-    private const string _consoleLogTemplate = "[{Timestamp:HH:mm:ss.fff} [{Level}] {SourceContext} {Message}{NewLine}{Exception}";
+    private const string _consoleLogTemplate = "[{Timestamp:HH:mm:ss.fff zzz} [{Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}";
 
     public static void AddSerilogLogging(this IServiceCollection _)
     {
