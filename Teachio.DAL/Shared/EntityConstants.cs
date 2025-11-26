dc0ce1f86dda1ec256514777841b4e6c0eacd73f
Michael-Kolpakov <mihailkolp@gmail.com>
2025-11-27 00:15:01 +0200

    Added entity constants

--- Teachio.DAL/Shared/EntityConstants.cs
+++ Teachio.DAL/Shared/EntityConstants.cs
@@ -3,4 +3,8 @@
 public static class EntityConstants
 {
     public const int MaxVideoDurationSeconds = 3600;
+
+    public const int MaxSectionsPerCourse = 35;
+
+    public const int MaxVideosPerSection = 40;
 }
