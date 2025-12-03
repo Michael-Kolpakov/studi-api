8e8e68e15cfb5ea49fc17e4cdd5d1b19e8e314ed
Michael-Kolpakov <mihailkolp@gmail.com>
2025-12-03 02:36:39 +0200

    Implemented TotalDurationResolver

---
+++ Teachio.BLL/Utils/MappingResolvers/TotalDurationResolver.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Teachio.BLL.Dto.Courses.Courses.Response;
+using Teachio.DAL.Entities.Courses.Courses;
+
+namespace Teachio.BLL.Utils.MappingResolvers;
+
+public class TotalDurationResolver : IValueResolver<Course, CoursePreviewResponseDto, float>
+{
+    public float Resolve(Course source, CoursePreviewResponseDto destination, float destMember, ResolutionContext context)
+    {
+        var durationSeconds = source.Sections
+            .SelectMany(s => s.Videos)
+            .Sum(v => v.DurationSeconds);
+
+        var durationHours = durationSeconds / 3600f;
+
+        return MathF.Round(durationHours, 1);
+    }
+}
