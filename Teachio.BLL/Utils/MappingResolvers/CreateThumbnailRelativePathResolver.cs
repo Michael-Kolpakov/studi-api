05b9b987b579e67e9a84c9d0be7032323e88bb9b
Michael-Kolpakov <mihailkolp@gmail.com>
2025-12-03 02:36:31 +0200

    Implemented CreateThumbnailRelativePathResolver

---
+++ Teachio.BLL/Utils/MappingResolvers/CreateThumbnailRelativePathResolver.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Teachio.BLL.Dto.Courses.Courses.Response;
+using Teachio.BLL.Utils.Constants;
+using Teachio.DAL.Entities.Courses.Courses;
+
+namespace Teachio.BLL.Utils.MappingResolvers;
+
+public class CreateThumbnailRelativePathResolver : IValueResolver<Course, CoursePreviewResponseDto, string>
+{
+    public string Resolve(Course source, CoursePreviewResponseDto destination, string destMember, ResolutionContext context)
+    {
+        return HandlerConstants.ThumbnailRelativePathTemplate
+            .Replace("{AppUser}", source.OwnerUser.Email)
+            .Replace("{CourseName}", source.CourseName)
+            .Replace("{ThumbnailName}", source.ThumbnailName);
+    }
+}
