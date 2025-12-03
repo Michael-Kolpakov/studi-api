027e5abe466e489e52999579d9bce2d16869e8e5
Michael-Kolpakov <mihailkolp@gmail.com>
2025-12-03 02:36:46 +0200

    Implemented TrimTitleResolver

---
+++ Teachio.BLL/Utils/MappingResolvers/TrimTitleResolver.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Teachio.BLL.Dto.Courses.Courses.Request;
+using Teachio.BLL.Dto.Courses.Sections.Request;
+
+namespace Teachio.BLL.Utils.MappingResolvers;
+
+public class TrimTitleResolver : IValueResolver<object, object, string>
+{
+    public string Resolve(object source, object destination, string destMember, ResolutionContext context)
+    {
+        return source switch
+        {
+            CourseCreateUpdateRequestDto courseSource => FormatTitle(courseSource.Title),
+            SectionCreateUpdateRequestDto sectionSource => FormatTitle(sectionSource.Title),
+            _ => throw new ArgumentException($"Unknown source '{nameof(source)}' type", nameof(source))
+        };
+    }
+
+    private static string FormatTitle(string title) =>
+        title.Trim();
+}
