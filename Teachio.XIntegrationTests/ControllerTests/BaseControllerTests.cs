de82f47f924519c9ef6b2e67743ffc31f9ff49f2
Michael-Kolpakov <mihailkolp@gmail.com>
2025-12-19 08:51:17 +0200

    Implemented integration tests for Course endpoints

---
+++ Teachio.XIntegrationTests/ControllerTests/BaseControllerTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Teachio.DAL.Persistence;
+using Teachio.DAL.Utils.Constants;
+using Teachio.DAL.Utils.Database;
+using Teachio.WebApi;
+using Teachio.XIntegrationTests.Base;
+using Teachio.XIntegrationTests.Utils;
+using Teachio.XIntegrationTests.Utils.Clients;
+using Teachio.XIntegrationTests.Utils.Helpers;
+
+namespace Teachio.XIntegrationTests.ControllerTests;
+
+public abstract class BaseControllerTests<TClient> : IntegrationTestBase, IClassFixture<CustomWebApplicationFactory<Program>>, IDisposable
+{
+    private bool _disposed;
+
+    protected TClient Client { get; set; }
+
+    protected BaseControllerTests(CustomWebApplicationFactory<Program> factory, string secondPartUrl)
+    {
+        Client = ClientInitializer<TClient>.Initialize(factory.CreateClient(), secondPartUrl);
+    }
+
+    public static SqlDbHelper GetSqlDbHelper()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<TeachioDbContext>()
+            .UseInMemoryDatabase(DatabaseConstants.IntegrationTestsInMemoryDatabase, IntegrationTestDatabaseRoot.Root);
+
+        return new SqlDbHelper(optionsBuilder.Options);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing && this.Client is IDisposable disposableClient)
+        {
+            disposableClient.Dispose();
+        }
+
+        _disposed = true;
+    }
+}
+
+public class BaseControllerTests(CustomWebApplicationFactory<Program> factory, string secondPartUrl = "")
+    : BaseControllerTests<BaseClient>(factory, secondPartUrl);
